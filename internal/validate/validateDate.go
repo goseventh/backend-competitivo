06a1b85a5c6fa7b33ffb58445f508943fdc781a8
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-21 02:14:01 -0300

    :sparkles: feat: requisitos da api

--- internal/validate/validateDate.go
+++ internal/validate/validateDate.go
@@ -4,9 +4,9 @@ import "regexp"
 import "time"
 
 func IsDate(dateStr string) bool {
-	ISO8601DateRegexString := `^(19\d\d|20[0-2]\d)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$`
-	ISO8601DateRegex := regexp.MustCompile(ISO8601DateRegexString)
-	return ISO8601DateRegex.MatchString(dateStr)
+	dateRegex := `^(19\d\d|20[0-2]\d)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$`
+	regex := regexp.MustCompile(dateRegex)
+	return regex.MatchString(dateStr)
 }
 
 func IsDateValid(dateStr string) bool {
