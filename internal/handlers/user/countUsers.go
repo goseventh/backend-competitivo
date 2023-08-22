7b0165bf6cc080b87114b70fefc9844c4e36abae
brunoisaac27 <brunoisaac2717@gmail.com>
2023-08-22 11:47:37 -0300

    :sparkles: feat: adicionado retorno No Registers em /contagem-pessoas
    este commit adiciona o retorno de erro caso não haja nenhum registro de usuários, em vez
    de retornar internal error

--- internal/handlers/user/countUsers.go
+++ internal/handlers/user/countUsers.go
@@ -5,6 +5,7 @@ import (
 	"main/database/mongo"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func HandlerCountUsers(c *fiber.Ctx) error {
@@ -12,10 +13,14 @@ func HandlerCountUsers(c *fiber.Ctx) error {
 	db.UseCollection("users")
 	users, _, err := db.CountUsers()
 	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			c.SendString("❌ no registers")
+      return c.SendStatus(fiber.StatusNotFound)
+		}
 		c.SendString("❌ internal error")
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-return c.SendString(fmt.Sprintf("👌 Total users: %v\n", users))
+	return c.SendString(fmt.Sprintf("👌 Total users: %v\n", users))
 
 }
