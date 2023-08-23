cd4be552c4df68b1b395d2d0dc5ef68b41d6f476
real-davidwilson <davidwilsonbr2019@gmail.com>
2023-08-22 23:59:07 -0300

    docker config

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
-FROM golang:1.20.7-alpine
-
+FROM golang:1.20.6
 
 WORKDIR /usr/src/app 
+
 COPY . .
+
 RUN go mod tidy
 RUN go build -o /usr/local/bin/app cmd/*.go
-EXPOSE 3000
-CMD ["sh", "-c", "/usr/local/bin/app"]
 
+EXPOSE 3000
+
+WORKDIR /usr/local/bin
+
+CMD ["sh", "-c", "app"]
--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	mongodb "main/database/mongo"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
-		Prefork:       true,
-		CaseSensitive: true,
-		StrictRouting: true,
-    StreamRequestBody: true,
-    Concurrency: 256*2048,
-		ServerHeader:  "github.com/goseventh",
-		AppName:       "goseventh backend",
-    ColorScheme: fiber.DefaultColors,
-    
+		Prefork:           true,
+		CaseSensitive:     true,
+		StrictRouting:     true,
+		StreamRequestBody: true,
+		Concurrency:       256 * 2048,
+		ServerHeader:      "github.com/goseventh",
+		AppName:           "goseventh backend",
+		ColorScheme:       fiber.DefaultColors,
 	})
 	Routers(app)
-  mongodb.Connect()
-	app.Listen(":3000")
+	mongodb.Connect()
+	app.Listen(os.Getenv("SERVER_PORT"))
 }
